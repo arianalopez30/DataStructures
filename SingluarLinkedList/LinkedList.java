5a731dc46b3d2ba798d78be0d939355d866c10ed
arianalopez30 <arianalopez30@hotmail.com>
2020-05-02 17:51:56 -0400

    Update LinkedList.java

--- SingluarLinkedList/LinkedList.java
+++ SingluarLinkedList/LinkedList.java
@@ -1,196 +1,386 @@
-
+//Implementing a Singular Linked List
 public class LinkedList {
 	
-	Node head  = null;
-	Node tail = null;
+	Node head = null;
 	public int size = 0;
 	
-	
 	//=========================================
-	//Name: constructor()
-	//Description: default constructor
+	//Name: LinkedList() default Constructor
+	//Description: Constructor that does nothing
 	//=========================================
 	public LinkedList() {
 		
 	}
-
+	
 	//=========================================
-	//Name: constructor(Object element)
-	//Description: default constructor adds a head
+	//Name: LinkedList(Object element) 
+	//Description: Constructor that starts linked
+	//list
 	//=========================================
 	public LinkedList(Object element) {
 		
 		head = new Node(element);
-		tail = head;
-		size ++;
+		size++;
 		
 	}
 	
 	//=========================================
 	//Name: add(Object element)
-	//Description: adds an element to LL
+	//Description: adds an element to linked list
 	//=========================================
 	public void add(Object element) {
 		
-		if(head == null) {
+		if(size == 0) {
 			head = new Node(element);
-			tail = head;
-			size++;
 		}
 		else {
 			
-			Node temp = new Node(element);
-			temp.next = tail.next;
-			tail.next = temp;
-			tail = temp;
-			size ++;			
+			Node current = head;
 			
+			while(current.next != null) {
+				
+				current = current.next;
+				
+			}
+			
+			current.next = new Node(element);
 		}
+		size++;
+		
 	}
-		
+	
 	//=========================================
-	//Name: addFirst(Object element)
-	//Description: add a new object at head
+	//Name: add(Object element)
+	//Description: adds an element to linked list
+	//at a specific index
 	//=========================================
-	public void addFirst(Object element) {
+	public void add(int index, Object element) {
 		
-		if (head == null) {
-			head = new Node(element);
-			tail = head;
-			size++;
+		if( (index < 0) || index >= size) {
+			
+			//do nothing
+			
 		}
-		else {
+		else if (index == 0) {
 			
 			Node temp = new Node(element);
 			temp.next = head;
 			head = temp;
-			tail.next = head;
-			size ++;
+			size++;
+			
+		}
+		else {
+			Node current = head;
+			for(int counter = 1; counter < index; counter ++){
+				
+				current = current.next;
+				
+			}
+			Node temp = new Node(element);
+			temp.next = current.next;
+			current.next = temp;
+			size++;
+			
 		}
 		
+		
+		
+		
+		
+	}
+	
+	//=========================================
+	//Name: print()
+	//Description: prints elements of the linked
+	//list
+	//=========================================
+	public String print() {
+		
+		Node current = head;
+		String output = "Here is the contents of my linked list: ";
+		
+		
+		while(current.next != null) {
+			output += " " + current.element;
+			current = current.next;
+		}
+		//for(int index = 0; index < size; index ++) {
+		//	output += " " + current.element;
+		//	current = current.next;
+			
+		//}
+		
+		output += " " + current.element;
+		
+		return output;
 		
 	}
 	
 	
 	//=========================================
 	//Name: remove()
-	//Description: removing the tail
+	//Description: remove element from linked list
 	//=========================================
 	public Object remove() {
 		
-		if(size == 0) {
+		Node temp = null;
+		Node current = head;
+		
+		for(int index = 0; index < size - 2; index ++) {
+			
+			current = current.next;
+			
+		}
+		temp = current.next;
+		current.next = null;
+		
+		size --;
+		
+		return temp.element;
+		
+	}
+	
+	
+	//=========================================
+	//Name: remove(int index)
+	//Description: remove an element to linked list
+	//at a specific index
+	//=========================================
+	public Object remove(int index) {
+		
+		Node temp = null;
+		Node current = head;
+
+		
+		if((index < 0) || (index >= size)) {
+			//out of range
+			return null;
+			
+		}
+		else if(index == 0) {
+			temp = head;
+			head = head.next;
+			size --;
+		}
+		else {
+		
+			for(int count = 1; count < index; count ++) {
+				
+				current = current.next;
+				
+			}
+			temp = current.next;
+			current.next = temp.next;
+			
+			size --;
+			
+		}
+		return temp.element;
+		
+	}
+	
+	
+	//=========================================
+	//Name: clear()
+	//Description: clears the list
+	//=========================================
+	public void clear() {
+		head = null;
+	}
+
+	//=========================================
+	//Name: getFirst()
+	//Description: gets the first element of the
+	//linked list
+	//=========================================
+	public Object getFirst() {
+		if (head ==null) {
 			return null;
 		}
-		if(size == 1 ) {
-			Node temp = tail;
-			tail = null;
+		return head.element;
+	}
+	
+	//=========================================
+	//Name: getLast()
+	//Description: gets the last element of the
+	//linked list
+	//=========================================
+	public Object getLast() {
+		
+		if (head == null) {
+			return null;
+		}
+		
+		Node current = head;
+		
+		while(current.next != null) {
+			current = current.next;
+		}
+		
+		return current.element;
+		
+	}
+	
+	
+	//=========================================
+	//Name: addFirst(int element)
+	//Description: adds an element at the beginning
+	//of the list
+	//=========================================
+	public void addFirst(int element) {
+		
+		if(head == null) {
+			head = new Node(element);
+		}
+		else {
+			Node temp = new Node(element);
+			temp.next = head;
+			head = temp;
+			
+		}
+		size ++;
+					
+	}
+	
+	
+	//=========================================
+	//Name: addLast(int element)
+	//Description: adds an element at the end
+	//of the list
+	//=========================================
+	public void addLast(int element) {
+		
+		if (head == null) {
+			head = new Node(element);
+		}
+		else {
+			
+			Node current = head;
+			while(current.next != null) {
+				current = current.next;
+			}
+			
+			current.next = new Node(element);
+		}
+		
+		size ++;
+		
+		
+	}
+	
+	//=========================================
+	//Name: removeFirst()
+	//Description: removes the first element
+	//of the list
+	//=========================================
+	public Object removeFirst() {
+		if(head == null)
+		{
+			return null;
+		}
+		if(size == 1){
+			Node temp = head;
 			head = null;
 			size --;
 			return temp.element;
 		}
 		else {
-			Node current = head;
-			for(int i = 1; i < size -1; i++) {
-				current = current.next;
-			}
-			Node temp = tail;
-			
-			current.next = tail.next;
-			tail = current;
-			
+			Node temp = head;
+			head = head.next;;
 			size --;
 			return temp.element;
 			
-			
+		}
+	}
+	
+	
+	//=========================================
+	//Name: removeLast()
+	//Description: removes the last element
+	//of the list
+	//=========================================
+	public Object removeLast() {
+		
+		Node current = head;
+		for(int i = 1; i < size -1; i++) {
+			current = current.next;
 		}
 		
+		Node temp = current.next;
+		current.next = null;
+		size --;
+		
+		return temp.element;
+
 	}
 	
 	//=========================================
 	//Name: removeIndex(int index)
-	//Description: remove element at specific 
-	//index
+	//Description: removes the element at a 
+	//specific index
 	//=========================================
 	public Object removeIndex(int index) {
 		
-		if(index < 0 || index >= size){
-			return null;
-		}
-		else {
-			Node current = head;
-			for(int i = 1; i < index; i ++) {
-				current = current.next;
-			}
-			Node temp = current.next;
-			
-			current.next = temp.next;
-			size --;
-			return temp.element;
-			
-		}
-		
-	}
-	
-	
-	//=========================================
-	//Name: toString()
-	//Description: print LL out
-	//=========================================
-	public String toString() {
-		
-		String output = "";
-		
-		Node current = head;
-		while(current != tail) {
-			output += current.element + " ";
-			current = current.next;
-		}
-		
-		output += tail.element;
-		
-		return output;
-	}
-	
-	//=========================================
-	//Name: getHead()
-	//Description: return head
-	//=========================================
-	public Object getHead() {
-		return head.element;
-	}
-	
-	//=========================================
-	//Name: getTail()
-	//Description: return tail
-	//=========================================
-	public Object getTail() {
-		return tail.element;
-	}
-
-	
-	//=========================================
-	//Name: getIndex()
-	//Description: get element at particular index
-	//=========================================
-	public Object getIndex(int index) {
-		
+		//check if index is within bounds
 		if(index < 0 || index >= size) {
 			return null;
 		}
-		else {
-			Node current = head;
-			for(int i = 1; i < index; i++) {
-				
-				current = current.next;
-				
-			}
-			
-			return current.next.element;
+		if(index == 0) {
+			removeFirst();
+		}
+		if(index == size -1) {
+			removeLast();
 		}
 		
+		Node current = head;
+		
+		for (int i = 1; i < index; i ++) {
+			current = current.next;
+		}
+		
+		Node temp = current.next;
+		current.next = temp.next;
+		size --;
+		
+		return temp.element;
 		
 		
 	}
+	
+	//=========================================
+	//Name: contains(Object element)
+	//Description: checks to see if the list
+	//contains this specific element
+	//=========================================
+	public boolean contains(Object element) {
+		
+		if (head == null) {
+			return false;
+		}
+		
+		Node current = head;
 
+		
+		for(int i =1; i <= size - 1; i ++) {
+
+			if(current.element == element) {
+				return true;
+			}
+			current = current.next;
+
+		}
+
+		if(current.element != element) {
+
+			return false;
+		}else {
+
+			return true;
+		}
+		
+		
+	}
+	
+	
 
 }
